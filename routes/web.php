87e065692a659de926927e0ba216831032e5e22d
Ikmalghz <fajrikmal141@gmail.com>
2023-06-02 15:00:00 +0700

    hak akses

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\TembakauController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,18 +18,20 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [LoginController::class, 'login'])->name('login');
 Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
-Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
 
 // Route::get('/', [LoginController::class, 'halamanLogin'])->name('login');
 // Route::post('/postLogin', [LoginController::class, 'postLogin'])->name('postLogin');
-Route::get('/index', [HomeController::class, 'index'])->name('index');
 // Route::get('/kasir', [HomeController::class, 'kasir'])->name('kasir');
-Route::get('/tembakau', [TembakauController::class, 'mbako'])->name('tembakau');
-Route::get('/tembakau/create', [TembakauController::class, 'create'])->name('create');
-Route::post('/tembakau/store', [TembakauController::class, 'store'])->name('store');
-Route::get('/tembakau/{id}/edit', [TembakauController::class, 'edit'])->name('edit');
-Route::put('/tembakau/{id}', [TembakauController::class, 'update'])->name('update');
-Route::delete('/tembakau/{id}', [TembakauController::class, 'destroy'])->name('destroy');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/index', [HomeController::class, 'index'])->name('index');
+    Route::get('/tembakau', [TembakauController::class, 'mbako'])->name('tembakau');
+    Route::get('/tembakau/create', [TembakauController::class, 'create'])->name('create');
+    Route::post('/tembakau/store', [TembakauController::class, 'store'])->name('store');
+    Route::get('/tembakau/{id}/edit', [TembakauController::class, 'edit'])->name('edit');
+    Route::put('/tembakau/{id}', [TembakauController::class, 'update'])->name('update');
+    Route::delete('/tembakau/{id}', [TembakauController::class, 'destroy'])->name('destroy');
+    Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
+});
 
 
 // Route::get('/update', function () {
